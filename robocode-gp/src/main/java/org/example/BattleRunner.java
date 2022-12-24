8c56e0f3f619fc823ece7f6828d71f3fd18a7905
yaskovdev <yaskovdev@gmail.com>
2022-12-24 22:21:54 +0200

    Cleanup.

--- robocode-gp/src/main/java/org/example/BattleRunner.java
+++ robocode-gp/src/main/java/org/example/BattleRunner.java
@@ -9,26 +9,28 @@ import robocode.control.RobotSpecification;
 import java.io.File;
 
 public class BattleRunner {
-    public static void main(String[] args) {
-        System.setProperty("NOSECURITY", "true");
-        System.setProperty("robocode.options.battle.desiredTPS", "10000");
-//        System.setProperty("robocode.options.battle.desiredTPS", "27");
+    public static void main(final String[] args) {
+        allowRobotsReadExternalFiles();
         RobocodeEngine.setLogMessagesEnabled(false);
 
-        RobocodeEngine engine = new RobocodeEngine(new File("/Users/yaskovdev/robocode"));
+        final RobocodeEngine engine = new RobocodeEngine(new File("/Users/yaskovdev/robocode"));
         engine.addBattleListener(new BattleObserver());
         engine.setVisible(false);
-        int numberOfRounds = 1;
+        final int numberOfRounds = 1;
 
         for (int i = 0; i < 70; i++) {
-            BattlefieldSpecification battlefield = new BattlefieldSpecification(800, 600);
-            RobotSpecification[] selectedRobots = engine.getLocalRepository("org.example.PushRobot,sample.Tracker");
-            BattleSpecification battleSpec = new BattleSpecification(numberOfRounds, battlefield, selectedRobots);
+            final BattlefieldSpecification battlefield = new BattlefieldSpecification(800, 600);
+            final RobotSpecification[] selectedRobots = engine.getLocalRepository("org.example.PushRobot,sample.Tracker");
+            final BattleSpecification battleSpec = new BattleSpecification(numberOfRounds, battlefield, selectedRobots);
             engine.runBattle(battleSpec, true);
             System.out.println("Battle " + i + " is over");
         }
 
         engine.close();
         System.exit(0);
+    }
+
+    private static void allowRobotsReadExternalFiles() {
+        System.setProperty("NOSECURITY", "true");
     }
 }
